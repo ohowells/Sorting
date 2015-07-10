7e81d694b253c6fe5db7d43e3cea5714d91d1896
ohowells <ohowells1@gmail.com>
2015-07-10 13:14:42 +0100

    Added Simple bubble sort program

---
+++ Bubble-sort/main.cpp
@@ -0,0 +1,36 @@
+# include <iostream>
+
+template<typename T>
+void BubbleSort(T arr[], const short& n)
+{
+    short temp = 0;
+    for (auto i = 0; i < n; i++)
+    {
+        for(auto j = 0; j < n-1; j++)
+        {
+            if(arr[j] < arr[i])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+template<typename T>
+static void PrintArray(const T arr[], const short& n)
+{
+    for (auto i = 0; i < n; i++)
+        std::cout << arr[i] << std::endl;
+}
+
+int main(void)
+{
+    int arr[] = { 5, 4, 12, 36, 1, 9, 11, 24, 100, 50 };
+
+    BubbleSort(arr, 10);
+    PrintArray(arr, 10);
+
+    return 0;
+}
